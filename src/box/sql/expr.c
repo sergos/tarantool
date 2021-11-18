05fa47b303e14bd3375eca91cbf84161275b529e
Mergen Imeev <imeevma@gmail.com>
2021-11-18 11:02:20 +0300

    sql: properly check bind variable names
    After this patch, variable names will have to follow the rules defined
    for identifiers in SQL. This essentially means that the number will no
    longer be used as the first character of the bind variable name.
    
    Part of #4763

--- src/box/sql/expr.c
+++ src/box/sql/expr.c
@@ -1314,6 +1314,59 @@ sqlExprAssignVarNumber(Parse * pParse, Expr * pExpr, u32 n)
 	}
 }
 
+struct Expr *
+expr_new_variable(struct Parse *parse, const struct Token *spec,
+		  const struct Token *id)
+{
+	assert(spec != 0 && spec->n == 1);
+	uint32_t len = 1;
+	if (parse->parse_only) {
+		diag_set(ClientError, ER_SQL_PARSER_GENERIC_WITH_POS,
+			 parse->line_count, parse->line_pos,
+			 "bindings are not allowed in DDL");
+		parse->is_aborted = true;
+		return NULL;
+	}
+	if (id != NULL) {
+		assert(spec->z[0] != '?');
+		if (id->z - spec->z != 1) {
+			diag_set(ClientError, ER_SQL_UNKNOWN_TOKEN,
+				 parse->line_count, spec->z - parse->zTail + 1,
+				 spec->n, spec->z);
+			parse->is_aborted = true;
+			return NULL;
+		}
+		if (spec->z[0] == '#' && (id != NULL && sqlIsdigit(id->z[0]))) {
+			diag_set(ClientError, ER_SQL_SYNTAX_NEAR_TOKEN,
+				 parse->line_count, spec->n, spec->z);
+			parse->is_aborted = true;
+			return NULL;
+		}
+		len += id->n;
+	}
+	struct Expr *expr = sqlDbMallocRawNN(parse->db,
+					     sizeof(*expr) + len + 1);
+	if (expr == NULL) {
+		parse->is_aborted = true;
+		return NULL;
+	}
+	memset(expr, 0, sizeof(*expr));
+	expr->type = FIELD_TYPE_BOOLEAN;
+	expr->op = TK_VARIABLE;
+	expr->flags = EP_Leaf;
+	expr->iAgg = -1;
+	expr->u.zToken = (char *)(expr + 1);
+	expr->u.zToken[0] = spec->z[0];
+	if (id != NULL)
+		memcpy(expr->u.zToken + 1, id->z, id->n);
+	expr->u.zToken[len] = '\0';
+	assert(SQL_MAX_EXPR_DEPTH > 0);
+	expr->nHeight = 1;
+
+	sqlExprAssignVarNumber(parse, expr, len);
+	return expr;
+}
+
 /*
  * Recursively delete an expression tree.
  */
