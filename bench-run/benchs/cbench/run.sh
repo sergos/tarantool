3383fa1a5ec0be4b1450dba310fd60d187ade761
VitaliyaIoffe <vitaioffe@yandex.ru>
2021-12-07 11:56:13 +0300

    bench-run: migrate and add publish for metrics
    bench-run was presented as separate repo, migrate it to tarantool due to
    avoiding execution perf in containers.
    Add publishing infrastructure to post a data to database associated with
    grafana.

---
+++ bench-run/benchs/cbench/run.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+set -eu
+set -o pipefail
+
+TAR_VER=$(tarantool -v | grep -e "Tarantool" |  grep -oP '\s\K\S*')
+numaopts="numactl --membind=1 --cpunodebind=1 --physcpubind=11"
+cbench_opts=500
+
+killall tarantool 2>/dev/null || true
+rm -rf 5* 0*
+sync && echo "sync passed" || echo "sync failed with error" $?
+echo 3 > /proc/sys/vm/drop_caches
+$numaopts tarantool /opt/cbench/cbench_runner.lua memtx 2>&1 | tee cbench_output_memtx.txt
+
+killall tarantool 2>/dev/null || true
+rm -rf 5* 0*
+sync && echo "sync passed" || echo "sync failed with error" $?
+echo 3 > /proc/sys/vm/drop_caches
+$numaopts tarantool /opt/cbench/cbench_runner.lua vinyl fsync $cbench_opts 2>&1 | tee cbench_output_vinyl_fsync.txt
+
+killall tarantool 2>/dev/null || true
+rm -rf 5* 0*
+sync && echo "sync passed" || echo "sync failed with error" $?
+echo 3 > /proc/sys/vm/drop_caches
+$numaopts tarantool /opt/cbench/cbench_runner.lua vinyl write $cbench_opts 2>&1 | tee cbench_output_vinyl_write.txt
+
+grep "^?tab=cbench.tree" cbench_output_memtx.txt | \
+  sed "s/.*name=//"| sed "s/&param=/:/"| sed "s/cb\./cb\.memtx\./"| \
+  tee -a cbench-memtx-tree_result.txt
+grep "^?tab=cbench.hash" cbench_output_memtx.txt | \
+  sed "s/.*name=//"| sed "s/&param=/:/"| sed "s/cb\./cb\.memtx\./"| \
+  tee -a cbench-memtx-hash_result.txt
+grep "^?tab" cbench_output_vinyl_fsync.txt | \
+  sed "s/.*name=//"| sed "s/&param=/:/"| sed "s/cb\./cb\.vinyl\.fsync\./"| \
+  tee -a cbench-vinyl-fsync_result.txt
+grep "^?tab" cbench_output_vinyl_write.txt | \
+  sed "s/.*name=//"| sed "s/&param=/:/"| sed "s/cb\./cb\.vinyl\.write\./"| \
+  tee -a cbench-vinyl-write_result.txt
+
+echo ${TAR_VER} | tee cbench-memtx-tree_t_version.txt
+echo ${TAR_VER} | tee cbench-memtx-hash_t_version.txt
+echo ${TAR_VER} | tee cbench-vinyl-fsync_t_version.txt
+echo ${TAR_VER} | tee cbench-vinyl-write_t_version.txt
+echo ${TAR_VER} | tee cbench_t_version.txt
+
+echo "Tarantool TAG:"
+cat cbench_t_version.txt
+echo "Overall results:"
+echo "================"
+echo "RESULTS (cbench-memtx-tree_result.txt):"
+cat cbench-memtx-tree_result.txt
+echo " "
+echo "RESULTS (cbench-memtx-hash_result.txt):"
+cat cbench-memtx-hash_result.txt
+echo " "
+echo "RESULTS (cbench-vinyl-fsync_result.txt):"
+cat cbench-vinyl-fsync_result.txt
+echo " "
+echo "RESULTS (cbench-vinyl-write_result.txt):"
+cat cbench-vinyl-write_result.txt
---
+++ bench-run/publish/publish.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+import argparse
+import fnmatch
+import logging
+import os
+from typing import AnyStr
+from typing import List
+
+import requests
+
+from metrics import Metric
+
+URL = os.getenv("INFLUXDB_BUCKET_URL")
+
+
+def parse_bench(filename: str) -> List[AnyStr]:
+    with open(filename) as raw_data:
+        return raw_data.readlines()
+
+
+def get_version(filename: str) -> str:
+    """Get tarantool version from the file
+
+    Version is provided by benchmarks in a file <Benchmark>_t_version.txt.
+    Example: Sysbench_t_version.txt includes '2.10.0-beta1-113-g16f7bf1'.
+    """
+    with open(filename) as raw_data:
+        version = raw_data.readlines()[-1]
+    version = version.split()[0]
+    if not version:
+        raise Exception("There was no version in a version file.")
+    return version
+
+
+def post_to_database(metric: Metric) -> None:
+    """Post request to the database."""
+    response = requests.post(
+        URL, data=str(metric),
+        headers={'Authorization': f'Token {os.environ["INFLUXDB_TOKEN"]}'}
+    )
+
+    logging.info(f"Sent metric: {metric} to the database. "
+                 f"Response is {response.status_code}")
+
+    if response.status_code >= 400:
+        raise Exception(f"Bad response: {response.status_code}")
+
+
+def main(directory: str):
+    logging.info(f"Files will be published from {directory}")
+    for file in os.listdir(directory):
+        if fnmatch.fnmatch(file, '*_result.txt'):
+            logging.info(f"{file} was matched as file with results")
+            values = parse_bench(os.path.join(directory, file))
+            benchmark = file.split('_')[0]
+            version = get_version(
+                os.path.join(directory, '{}_t_version.txt'.format(benchmark)))
+            logging.info(f"VERSION - {version}")
+            for value in values:
+                test_name = value.split(':')[0]
+                test_res = float(value.split(':')[1])
+                metric = Metric(benchmark=benchmark,
+                                values={test_name: test_res})
+                post_to_database(metric)
+
+    return 0
+
+
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-f", "--from-directory", required=True,
+                    help="directory with files to publish")
+    args = ap.parse_args()
+
+    logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
+
+    main(directory=args.from_directory)
